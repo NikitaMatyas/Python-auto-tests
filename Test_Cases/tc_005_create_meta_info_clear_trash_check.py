758a6d8287e62113adbbdc29816cc01afbe155b9
Nikita <matyasnikita94@gmail.com>
2019-08-15 18:04:15 +0300

    Закончил tc_005 с документацией Создал файл tc_004

--- Test_Cases/tc_005_create_meta_info_clear_trash_check.py
+++ Test_Cases/tc_005_create_meta_info_clear_trash_check.py
@@ -3,4 +3,34 @@ from framework_core.variables import disk_url, token
 
 
 def test_tc_005():
-    pass
+    """ Создать папки test->foo и файл внутри, сравнить тип параметров, удалить данные и проверить, что они удалены
+    1. Создание папки test на Яндекс диске (Код ответа == 201)
+    2. Создание папки foo внутри папки test (Код ответа == 201)
+    3. Получение URL для загрузки файла в папку test (Код ответа == 200, URL для загрузки)
+    4. Загрузка файла по сгенерированному URL (Код ответа == 201)
+    5. Получение данных о Яндекс диске и проверка типов (Код ответа == 200, типы совпадают)
+    6. Удаление папки test со всеми вложенными ресурсами (Код ответа == 204 или 200)
+    7. Проверка, что папка test была удалена (Код ответа == 404)
+    """
+    data = create_folder(disk_url, token, '%2Ftest')
+    assert data.status_code == 201, 'Не удалось создать папку на Яндекс диске'
+
+    data = create_folder(disk_url, token, '%2Ftest%2Ffoo')
+    assert data.status_code == 201, 'Не удалось создать папку на Яндекс диске'
+
+    data, url = file_upload_link(disk_url, token, '%2Ftest%2Ffoo%2Fautotest')
+    assert data.status_code == 200, 'Не удалось получить URL для загрузки файла на Яндекс диск'
+
+    data = file_upload(url)
+    assert data.status_code == 201, 'Не удалось загрузить файл по сгенерированному URL'
+
+    data = check_resource(disk_url, token, '%2ftest')
+    assert data.status_code == 200, 'Не удалось получить данные о Яндекс диске'
+    assert data.json()["type"] == "dir"
+    assert data.json()["_embedded"]["items"][0]["type"] == "dir"
+
+    data = delete_resource(disk_url, token, '%2Ftest', 'true', 1)
+    assert data == 204 or data == 200
+
+    data = check_resource(disk_url, token, '%2Ftest')
+    assert data.status_code == 404, 'Папка существует на диске или не удалось получить данные'
---
+++ Test_Cases/tc_006_create_folders_file_clear_trash_check.py
@@ -0,0 +1,3 @@
+from business_layer.yandex.yandex_requests import *
+from framework_core.variables import disk_url, token
+
